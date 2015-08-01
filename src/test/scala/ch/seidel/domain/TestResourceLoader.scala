870e5fc27a13990839dbe4024d6d77602c15ec61
luechtdiode <roland.seidel@interpolar.ch>
2015-08-02 01:03:33 +0200

    csv dataloader

---
+++ src/test/scala/ch/seidel/domain/TestResourceLoader.scala
@@ -0,0 +1,46 @@
+package ch.seidel.domain
+
+import scala.io.Source
+import java.io.File
+import scala.annotation.tailrec
+
+/**
+ * @author Roland
+ */
+object TestResourceLoader extends App with KutuService {
+
+  val vereinCsv = Source.fromFile(new File("./db/verein_201508012103.csv")).getLines()
+  val vereinHeader = vereinCsv.take(1).map(_.dropWhile {_.isUnicodeIdentifierPart }).flatMap(parseLine).zipWithIndex.toMap
+  println(vereinHeader)
+  val vereinNameIdx = vereinHeader("name")
+  val vereinIdIdx = vereinHeader("id")
+  val vereinInstances = vereinCsv.map(parseLine).map{fields =>
+    println(fields.toList)
+    val candidate = Verein(id = 0, name = fields(vereinNameIdx))
+    val verein = insertVerein(candidate)
+    (fields(vereinIdIdx), verein)
+  }.toMap
+  println(vereinInstances.toList)
+
+  val athletCsv = Source.fromFile(new File("./db/athlet_201508012323.csv")).getLines()
+  val athletHeader = athletCsv.take(1).map(_.dropWhile {_.isUnicodeIdentifierPart }).flatMap(parseLine).zipWithIndex.toMap
+  println(athletHeader)
+  val athletInstances = athletCsv.map(parseLine).map{fields =>
+    println(fields.toList)
+    val candidate = Athlet(
+        id = 0,
+        js_id = fields(athletHeader("js_id")),
+        geschlecht = fields(athletHeader("geschlecht")),
+        name = fields(athletHeader("name")),
+        vorname = fields(athletHeader("vorname")),
+        gebdat = Some(fields(athletHeader("gebdat"))),
+        strasse = fields(athletHeader("strasse")),
+        plz = fields(athletHeader("plz")),
+        ort = fields(athletHeader("ort")),
+        verein = Some(vereinInstances(fields(athletHeader("verein"))).id),
+        activ = fields(athletHeader("activ")).toUpperCase() match {case "TRUE" => true case _ => false}
+        )
+    val athlet = insertAthlete(candidate)
+    (fields(athletHeader("id")), athlet)
+  }.toMap
+}
