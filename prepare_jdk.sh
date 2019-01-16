1dd39417c866eeb54ee94932f6a332ffbe08728e
Roland Seidel <register@interpolar.ch>
2019-01-16 07:41:52 +0100

    fix os-names for Liux and Win64

--- prepare_jdk.sh
+++ prepare_jdk.sh
@@ -5,18 +5,18 @@
 
 # https://mail.openjdk.java.net/pipermail/openjfx-dev/2018-September/022500.html
 
-if [ ${OS} == 'win64' ]
+if [ ${OS} == 'Win64' ]
 then
-#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.zip -o "jdk-win64.zip"
-    curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-windows.zip -o "javapackager-win64.zip"
+#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.zip -o "jdk-Win64.zip"
+    curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-windows.zip -o "javapackager-Win64.zip"
     curl -L https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe -o "wix311.exe"
 #    wix311.exe /install /quiet /norestart
 fi
 
-if [ ${OS} == 'linux' ]
+if [ ${OS} == 'Linux' ]
 then
-#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_linux_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.tar.gz -0 "jdk-linux.zip"
-    curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-linux.zip -o "javapackager-linux.zip"
+#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_linux_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.tar.gz -0 "jdk-Linux.zip"
+    curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-linux.zip -o "javapackager-Linux.zip"
 fi
 
 if [ ${OS} == 'Darwin' ]
