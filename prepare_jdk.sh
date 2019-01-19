2cfa8a06b94dae0016cd82439491eab173d5ebb0
Roland Seidel <register@interpolar.ch>
2019-01-19 13:20:01 +0100

    fix windows build: try get jdk via choco install

--- prepare_jdk.sh
+++ prepare_jdk.sh
@@ -7,13 +7,13 @@
 
 if [ ${OS} == 'Win64' ]
 then
-    # curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.zip -o "jdk-Win64.zip"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.zip -o "jdk-Win64.zip"
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-windows.zip -o "javapackager-Win64.zip"
 fi
 
 if [ ${OS} == 'MSYS_NT-10.0' ]
 then
-#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.zip -o "jdk-${OS}.zip"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.zip -o "jdk-${OS}.zip"
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-windows.zip -o "javapackager-${OS}.zip"
 #    unzip -u "jdk-${OS}.zip" -d jdk
 #    mv jdk/*/ jdk11/
@@ -23,27 +23,28 @@ fi
 
 if [ ${OS} == 'Linux' ]
 then
-#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_linux_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.tar.gz -0 "jdk-Linux.zip"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_linux_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.tar.gz -0 "jdk-Linux.zip"
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-linux.zip -o "javapackager-Linux.zip"
 fi
 
 if [ ${OS} == 'Darwin' ]
 then
-#    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_mac_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.tar.gz -o "jdk-Darwin.zip"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_mac_openj9_jdk-11.0.1_13_openj9-0.11.0_11.0.1_13.tar.gz -o "jdk-Darwin.zip"
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-osx.zip -o "javapackager-Darwin.zip"
 fi
 
-#unzip -u "jdk-${OS}.zip" -d jdk
-#
-#mv jdk/*/ jdk11/
-#
-#unzip -u "javapackager-${OS}.zip" -d "jdk11/bin"
+unzip -u "jdk-${OS}.zip" -d jdk
+
+mv jdk/*/ jdk11/
+
+export JAVA_HOME="${PWD}/jdk11"
+
+export PATH=${JAVA_HOME}/bin:$PATH
+
+echo "${JAVA_HOME}"
+
+unzip -u "javapackager-${OS}.zip" -d "jdk11/bin"
 unzip -u "javapackager-${OS}.zip" -d "${JAVA_HOME}/bin"
 
 cp "${JAVA_HOME}/bin/jdk.packager.jar" "${JAVA_HOME}/jmods"
 
-#export JAVA_HOME="${PWD}/jdk11"
-#
-#export PATH=${JAVA_HOME}/bin:$PATH
-#
-echo "${JAVA_HOME}"
