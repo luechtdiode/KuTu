0d8b486af003ad59f8e3feba5b0c1f67bdda7bad
Roland Seidel <register@interpolar.ch>
2019-01-20 11:26:09 +0100

    activate all stages

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
+region = eu-west-1
 aws_default_region = eu-west-1
 aws_access_key_id = ${aws_user}
 aws_secret_access_key = ${aws_accesskey}
