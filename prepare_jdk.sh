1af9c6e05d70b0d192bfb8efa1e375d621259a94
Roland Seidel <register@interpolar.ch>
2019-04-29 01:12:08 +0200

    fix jdk-qualifier subdirectory

--- prepare_jdk.sh
+++ prepare_jdk.sh
@@ -38,7 +38,7 @@ fi
 
 if [ ${OS} == 'Linux' ]
 then
-    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.0.tar.gz -0 "jdk-${OS}.tar.gz"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.0.tar.gz -o "jdk-${OS}.tar.gz"
     tar -xzf "jdk-${OS}.tar.gz" -C jdk
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-linux.zip -o "javapackager-${OS}.zip"
     mv jdk/*/ jdk11/
@@ -56,8 +56,8 @@ then
     tar -xzf "jdk-${OS}.tar.gz" -C jdk
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-osx.zip -o "javapackager-${OS}.zip"
     echo "download jdk11 for ${OS} finished"
-    unzip -u "javapackager-${OS}.zip" -d jdk/Contents/Home/bin
-    cp jdk/Contents/Home/bin/jdk.packager.jar jdk/Contents/Home/jmod
+    unzip -u "javapackager-${OS}.zip" -d jdk/jdk-11.0.3+7/Contents/Home/bin
+    cp jdk/jdk-11.0.3+7/Contents/Home/bin/jdk.packager.jar jdk/jdk-11.0.3+7/Contents/Home/jmods
     sudo mv jdk/*/ /Library/Java/JavaVirtualMachines/jdk-11.jdk
     sudo ln -s /Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home jdk11
     export JAVA_HOME="${PWD}/jdk11"
