56bd9b018c1e12dd0a63e2295238adf9b208dba0
Roland Seidel <register@interpolar.ch>
2019-05-01 18:42:15 +0200

    patch javafx.scene.control.skin.TableCellSkin to prevent npe

---
+++ src/main/java/javafx/scene/control/skin/TableCellSkin.java
@@ -0,0 +1,70 @@
+package javafx.scene.control.skin;
+
+import com.sun.javafx.scene.control.behavior.BehaviorBase;
+import com.sun.javafx.scene.control.behavior.TableCellBehavior;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+
+public class TableCellSkin<S, T> extends TableCellSkinBase<S, T, TableCell<S, T>> {
+    private final BehaviorBase<TableCell<S, T>> behavior;
+
+    public TableCellSkin(TableCell<S, T> var1) {
+        super(var1);
+        this.behavior = new TableCellBehavior(var1);
+    }
+
+    public void dispose() {
+        super.dispose();
+        if (this.behavior != null) {
+            this.behavior.dispose();
+        }
+
+    }
+
+    public ReadOnlyObjectProperty<TableColumn<S, T>> tableColumnProperty() {
+        TableCell skinnable = this.getSkinnable();
+        if (skinnable != null) {
+            return skinnable.tableColumnProperty();
+        } else {
+            return new ReadOnlyObjectProperty<TableColumn<S, T>>() {
+                @Override
+                public Object getBean() {
+                    return null;
+                }
+
+                @Override
+                public String getName() {
+                    return null;
+                }
+
+                @Override
+                public TableColumn<S, T> get() {
+                    return null;
+                }
+
+                @Override
+                public void addListener(ChangeListener<? super TableColumn<S, T>> changeListener) {
+
+                }
+
+                @Override
+                public void removeListener(ChangeListener<? super TableColumn<S, T>> changeListener) {
+
+                }
+
+                @Override
+                public void addListener(InvalidationListener invalidationListener) {
+
+                }
+
+                @Override
+                public void removeListener(InvalidationListener invalidationListener) {
+
+                }
+            };
+        }
+    }
+}
