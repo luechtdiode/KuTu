3b2e746be279d1ae385407b93027ea0da130b302
Roland Seidel <register@interpolar.ch>
2019-09-05 00:07:31 +0200

    bump to latest OpenJDK 11 with hotspot

--- prepare_jdk.sh
+++ prepare_jdk.sh
@@ -11,7 +11,7 @@ mkdir jdk
 
 if [ ${OS} == 'Win64' ]
 then
-    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_windows_openj9_11.0.3_7_openj9-0.14.0.zip -o "jdk-${OS}.zip"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11/OpenJDK11U-jdk_x64_windows_hotspot_11.0.4_11.zip -o "jdk-${OS}.zip"
     unzip -u "jdk-${OS}.zip" -d jdk  >/dev/null 2>&1
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-windows.zip -o "javapackager-${OS}.zip"
     mv jdk/*/ jdk11/  >/dev/null 2>&1
@@ -21,7 +21,7 @@ fi
 
 if [ ${OS} == 'MSYS_NT-10.0' ]
 then
-    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_windows_openj9_11.0.3_7_openj9-0.14.0.zip -o "jdk-${OS}.zip"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11/OpenJDK11U-jdk_x64_windows_hotspot_11.0.4_11.zip -o "jdk-${OS}.zip"
     unzip -u "jdk-${OS}.zip" -d jdk  >/dev/null 2>&1
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-windows.zip -o "javapackager-${OS}.zip"
     mv jdk/*/ jdk11/  >/dev/null 2>&1
@@ -31,7 +31,7 @@ fi
 
 if [ ${OS} == 'Linux' ]
 then
-    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.0.tar.gz -o "jdk-${OS}.tar.gz"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11/OpenJDK11U-jdk_x64_linux_hotspot_11.0.4_11.tar.gz -o "jdk-${OS}.tar.gz"
     tar -xzf "jdk-${OS}.tar.gz" -C jdk  >/dev/null 2>&1
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-linux.zip -o "javapackager-${OS}.zip"
     mv jdk/*/ jdk11/  >/dev/null 2>&1
@@ -42,12 +42,12 @@ fi
 if [ ${OS} == 'Darwin' ]
 then
     echo "install jdk11 for ${OS}"
-    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.3_7.tar.gz -o "jdk-${OS}.tar.gz"
+    curl -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.4%2B11.4/OpenJDK11U-jdk_x64_mac_hotspot_11.0.4_11.tar.gz -o "jdk-${OS}.tar.gz"
     tar -xzf "jdk-${OS}.tar.gz" -C jdk  >/dev/null 2>&1
     curl -L http://download2.gluonhq.com/jpackager/11/jdk.packager-osx.zip -o "javapackager-${OS}.zip"
     echo "download jdk11 for ${OS} finished"
-    unzip -u "javapackager-${OS}.zip" -d jdk/jdk-11.0.3+7/Contents/Home/bin  >/dev/null 2>&1
-    cp jdk/jdk-11.0.3+7/Contents/Home/bin/jdk.packager.jar jdk/jdk-11.0.3+7/Contents/Home/jmods
+    unzip -u "javapackager-${OS}.zip" -d jdk/jdk-11.0.4+11/Contents/Home/bin  >/dev/null 2>&1
+    cp jdk/jdk-11.0.4+11/Contents/Home/bin/jdk.packager.jar jdk/jdk-11.0.4+11/Contents/Home/jmods
     sudo mv jdk/*/ /Library/Java/JavaVirtualMachines/jdk-11.jdk
     sudo ln -s /Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home jdk11
 fi
