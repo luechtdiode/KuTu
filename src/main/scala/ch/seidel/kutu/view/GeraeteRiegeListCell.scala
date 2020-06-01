0b24b24836b639bea25658e34e84039ac1765238
Roland Seidel <register@interpolar.ch>
2020-06-01 19:14:35 +0200

    optimize imports

--- src/main/scala/ch/seidel/kutu/view/GeraeteRiegeListCell.scala
+++ src/main/scala/ch/seidel/kutu/view/GeraeteRiegeListCell.scala
@@ -1,46 +1,10 @@
 package ch.seidel.kutu.view
 
-import java.util.UUID
-import java.util.concurrent.{ScheduledFuture, TimeUnit}
-
-import ch.seidel.commons._
-import ch.seidel.kutu.Config._
-import ch.seidel.kutu.KuTuApp.enc
-import ch.seidel.kutu.akka._
 import ch.seidel.kutu.domain._
-import ch.seidel.kutu.http.WebSocketClient
-import ch.seidel.kutu.renderer.PrintUtil.FilenameDefault
-import ch.seidel.kutu.renderer._
-import ch.seidel.kutu.{Config, KuTuApp, KuTuServer}
 import javafx.scene.{control => jfxsc}
-import org.slf4j.LoggerFactory
-import scalafx.Includes._
-import scalafx.application.Platform
-import scalafx.beans.binding.Bindings
-import scalafx.beans.property.StringProperty.sfxStringProperty2jfx
-import scalafx.beans.property.{DoubleProperty, ReadOnlyStringWrapper, _}
-import scalafx.beans.value.ObservableValue
-import scalafx.collections.ObservableBuffer
 import scalafx.collections.ObservableBuffer.observableBuffer2ObservableList
-import scalafx.event.ActionEvent
-import scalafx.event.subscriptions.Subscription
-import scalafx.geometry._
-import scalafx.print.PageOrientation
-import scalafx.scene.Node
-import scalafx.scene.control.SelectionMode.sfxEnum2jfx
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control.TableView.sfxTableView2jfx
 import scalafx.scene.control._
 import scalafx.scene.image.{Image, ImageView}
-import scalafx.scene.input.{Clipboard, KeyEvent}
-import scalafx.scene.layout._
-import scalafx.util.converter.{DefaultStringConverter, DoubleStringConverter}
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scala.io.Source
-import scala.util.{Failure, Success}
 
 class GeraeteRiegeListCell extends ListCell[GeraeteRiege] {
   var okIcon: Image = null
