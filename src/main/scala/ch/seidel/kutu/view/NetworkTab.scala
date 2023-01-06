e789f9dbc6b6702183898e042b71dec6f4c13e37
Roland Seidel <register@interpolar.ch>
2023-01-06 23:09:08 +0100

    Performance optimizations

--- src/main/scala/ch/seidel/kutu/view/NetworkTab.scala
+++ src/main/scala/ch/seidel/kutu/view/NetworkTab.scala
@@ -322,8 +322,10 @@ class NetworkTab(wettkampfmode: BooleanProperty, override val wettkampfInfo: Wet
     model.setAll(items.asJavaCollection)
     isRunning.set(model.exists(_.getValue.isRunning))
     selected.foreach(selection => {
-      val column = view.getColumns.get(selection.column)
-      view.selectionModel.value.select(selection.row, column)
+      if (view.getColumns.size() > selection.column) {
+        val column = view.getColumns.get(selection.column)
+        view.selectionModel.value.select(selection.row, column)
+      }
     })
     updateButtons
   }
--- src/test/scala/ch/seidel/kutu/load/competition/SimulationBottmingenD1.scala
+++ src/test/scala/ch/seidel/kutu/load/competition/SimulationBottmingenD1.scala
@@ -151,7 +151,7 @@ class SimulationBottmingenD1 extends Simulation {
     .exec(BrowseResults.loadAndSaveDurchgaenge)
     .exec(BrowseResults.loadAndSaveGeraete)
     .exec(BrowseResults.diveToWertungen)
-    .exec(scnCloseWebsocketUser)
+    //.exec(scnCloseWebsocketUser)
   //    .pause(20 seconds, 30 seconds)
 
   setUp(
