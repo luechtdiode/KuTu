e6d94910f50e9ce2854ef3bcc4bb7346f31e0b48
luechtdiode <roland.seidel@interpolar.ch>
2023-05-28 17:36:12 +0200

    #698 add avg score-editor - refactor to form-control

--- newclient/resultcatcher/src/app/component/wertung-avg-calc/wertung-avg-calc.component.ts
+++ newclient/resultcatcher/src/app/component/wertung-avg-calc/wertung-avg-calc.component.ts
@@ -1,11 +1,36 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-wertung-avg-calc',
   templateUrl: './wertung-avg-calc.component.html',
   styleUrls: ['./wertung-avg-calc.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi:true,
+      useExisting: WertungAvgCalcComponent
+    }
+  ]
 })
-export class WertungAvgCalcComponent implements OnInit {
+export class WertungAvgCalcComponent implements ControlValueAccessor { 
+  static nextId = 0;
+
+  @HostBinding() id = `avg-calc-input-${WertungAvgCalcComponent.nextId++}`;
+  
+  _fixed: number = 3;
+  
+  get fixed(): number {
+    return this._fixed;
+  }
+
+  @Input()
+  set fixed(value: number) {
+    this._fixed = value;
+    this.calcAvg();
+    this.markAsTouched();
+  }
 
   @Input()
   hidden: boolean;
@@ -22,41 +47,37 @@ export class WertungAvgCalcComponent implements OnInit {
   @Input()
   valueDescription: string;
 
-  @Input()
-  singleValue: number;
-
-  @Output()
-  avgValue: EventEmitter<number> = new EventEmitter();
+  avgValue: number;
 
   singleValues: {value:number}[] = [];
-
-  ngOnInit() {
-    this.singleValues = [{value: this.singleValue}];
-  }
 
   get title(): string {
     return this.valueTitle;
   }
 
   get singleValueContainer() {
-    return this.singleValues[0];
+    return this.singleValues[0]?.value || this.avgValue;
+  }
+
+  set singleValueContainer(value: number) {
+    this.writeValue(value);
+    this.calcAvg();
+    this.markAsTouched();
   }
 
   add() {
-    this.singleValues = [...this.singleValues, {value: 0.000}];
+    if (!this.disabled) {
+      this.singleValues = [...this.singleValues, {value: 0.000}];
+      this.markAsTouched();
+    }
   }  
 
   remove(index) {
-    if (index > -1) {
+    if (!this.disabled && index > -1) {
       this.singleValues.splice(index, 1);
+      this.calcAvg();
+      this.markAsTouched();
     }
-  }
-
-  onChange(event, item) {
-    item.value = event.target.value;
-  }
-  onBlur(event, item) {
-    item.value = Number(event.target.value).toFixed(3);
   }
 
   calcAvg(): number {
@@ -64,8 +85,65 @@ export class WertungAvgCalcComponent implements OnInit {
       .filter(item => !!item.value)
       .map(item => Number(item.value))
       .filter(item => item > 0)
-    const avg2 = Number((avg1.reduce((sum, current) => sum + current, 0) / avg1.length).toFixed(3));
-    this.avgValue.emit(avg2);
+    const avg2 = Number((avg1.reduce((sum, current) => sum + current, 0) / avg1.length).toFixed(this.fixed));
+    if (!this.disabled && this.avgValue !== avg2) {
+      this.avgValue = avg2;
+      this.onChange(avg2);
+      console.log('value updated: ' + avg2);
+    }
     return avg2;
+  }
+
+  onTouched = () => {};
+  
+  onChange = (avgValue: number) => {};
+
+  onItemChange(event, item) {
+    item.value = event.target.value;
+    this.calcAvg();
+    this.markAsTouched();
+  }
+  
+  onBlur(event, item) {
+    item.value = Number(event.target.value).toFixed(this.fixed);
+    this.calcAvg();
+    this.markAsTouched();
+  }
+
+  touched = false;
+
+  disabled = false;
+
+  writeValue(avgValue: number) {
+    console.log('writValue ' + avgValue);
+    this.avgValue = avgValue;
+    this.singleValues = [{value: avgValue}];
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+  }
+
+  @ViewChild('noteInput') public noteInput: { setFocus: () => void; };
+  
+  focused = false;
+
+  setFocus() {
+    this.noteInput.setFocus();
   }
 }
