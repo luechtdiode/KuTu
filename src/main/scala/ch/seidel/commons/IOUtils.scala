3cb04bb76abcf94087a3408a41a49a798549f43d
Roland Seidel <register@interpolar.ch>
2023-11-12 19:35:27 +0100

    clean code: remove unused function in IOUtils

--- src/main/scala/ch/seidel/commons/IOUtils.scala
+++ src/main/scala/ch/seidel/commons/IOUtils.scala
@@ -6,16 +6,6 @@ import scala.util.control.NonFatal
 /** Helper functions related to I/O. */
 object IOUtils {
 
-  /** Load a resource as a string.
-    *
-    * @param reference using class loader of the `reference` object to load the resource
-    * @param path to the resource, relative to the reference, or absolute of it starts with `/`
-    */
-  def loadResourceAsString(reference: Any, path: String): String = {
-    val in = reference.getClass.getResourceAsStream(path)
-    Source.fromInputStream(in).mkString
-  }
-
   def withResources[T <: AutoCloseable, V](r: => T)(f: T => V): V = {
     val resource: T = r
     require(resource != null, "resource is null")
