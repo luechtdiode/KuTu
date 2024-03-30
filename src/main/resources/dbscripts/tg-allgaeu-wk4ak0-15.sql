69ca3b606cfa8bf9f3552868427331d9bf938aba
Roland Seidel <register@interpolar.ch>
2024-03-30 11:22:55 +0100

    #858 Allow KuTuRi TG Allgaeu WK IV LK2 starting with age between 0 and 15 - adapt start-age down to zero

--- src/main/resources/dbscripts/tg-allgaeu-wk4ak0-15.sql
+++ src/main/resources/dbscripts/tg-allgaeu-wk4ak0-15.sql
@@ -1,2 +1,4 @@
--- allow TG Allgaeu WK IV LK2 starting between 0 and 15 - adapt start-age down to zero
-UPDATE programm SET alter_von=0 WHERE uuid='f414e11d-9943-44c6-9267-99b4568bebb2'
+-- allow KuTu TG Allgaeu WK IV LK2 starting between 0 and 15 - adapt start-age down to zero
+UPDATE programm SET alter_von=0 WHERE uuid='f414e11d-9943-44c6-9267-99b4568bebb2';
+-- allow KuTuRi TG Allgaeu WK IV LK2 starting between 0 and 15 - adapt start-age down to zero
+UPDATE programm SET alter_von=0 WHERE uuid='7575f0ef-041f-40ee-980b-92586371b965'
