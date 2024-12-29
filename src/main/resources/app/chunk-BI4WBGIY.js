c6f4c8a076f8a164d529856ab761875960ca00c2
luechtdiode <luechtdiode@users.noreply.github.com>
2024-12-29 10:25:41 +0000

    update with generated Client from Github Actions CI for build  with [skip ci]

---
+++ src/main/resources/app/chunk-BI4WBGIY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YPCCWTCR.js";import"./chunk-N7UIYTQY.js";import"./chunk-426OJ4HC.js";import"./chunk-MM5QLNJM.js";import"./chunk-H3GX5QFY.js";import"./chunk-MCRJI3T3.js";import"./chunk-PL2JVG7G.js";import"./chunk-ACERUE7O.js";import"./chunk-2R6CW7ES.js";export{a as mdTransitionAnimation};
---
+++ src/main/resources/app/chunk-RERPZ73W.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-J5OMF6R4.js";import"./chunk-7HA5TF5T.js";import"./chunk-2R6CW7ES.js";export{b as iosTransitionAnimation,a as shadow};
