f1ffc29a646ed647aab16c2f7fc11a30d7ae85e0
Roland Seidel <register@interpolar.ch>
2025-06-26 15:02:44 +0200

    #994 concept and poc for templated score input

--- src/main/scala/ch/seidel/kutu/calc/ScoreCalcTemplate.scala
+++ src/main/scala/ch/seidel/kutu/calc/ScoreCalcTemplate.scala
@@ -5,9 +5,13 @@ import ch.seidel.kutu.http.JsonSupport
 import scala.math.BigDecimal.RoundingMode
 
 sealed trait ScoreAggregateFn
+
 case object Min extends ScoreAggregateFn
+
 case object Max extends ScoreAggregateFn
+
 case object Avg extends ScoreAggregateFn
+
 case object Sum extends ScoreAggregateFn
 
 object ScoreCalcVariable_ {
@@ -16,28 +20,61 @@ object ScoreCalcVariable_ {
     case _ => ScoreCalcVariable(source, prefix, name, scale, BigDecimal("0"))
   }
 }
+
 case class ScoreCalcVariable(source: String, prefix: String, name: String, scale: Option[Int], value: BigDecimal) {
   def updated(newValue: BigDecimal): ScoreCalcVariable = copy(value = newValue.setScale(value.scale, RoundingMode.HALF_UP)): ScoreCalcVariable
 }
+
 case object TemplateJsonReader extends JsonSupport {
+
   import spray.json.enrichString
-  def apply(text: String) = scoreCalcTemplateFormat.read(text.parseJson)
+
+  def apply(text: String): ScoreCalcTemplate = scoreCalcTemplateFormat.read(text.parseJson)
 }
+
+case class ScoreCalcTemplateView(
+                                  dExpression: String, dVariables: List[ScoreCalcVariable], dDetails: Boolean,
+                                  eExpression: String, eVariables: List[ScoreCalcVariable], eDetails: Boolean,
+                                  pExpression: String, pVariables: List[ScoreCalcVariable], pDetails: Boolean,
+                                  aggregateFn: Option[ScoreAggregateFn])
+
 case class ScoreCalcTemplate(dFormula: String, eFormula: String, pFormula: String, aggregateFn: Option[ScoreAggregateFn]) {
   private val varPattern = "\\$([DEP]{1})([\\w]+[\\w\\d]*)(.([0123]+))?".r
 
-  val dVariables = parseVariables(dFormula)
-  val dResolveDetails = dFormula.endsWith("^")
-  val eVariables = parseVariables(eFormula)
-  val eResolveDetails = eFormula.endsWith("^")
-  val pVariables = parseVariables(pFormula)
-  val pResolveDetails = pFormula.endsWith("^")
+  val dVariables: List[ScoreCalcVariable] = parseVariables(dFormula)
+  val dResolveDetails: Boolean = dFormula.endsWith("^")
+  val eVariables: List[ScoreCalcVariable] = parseVariables(eFormula)
+  val eResolveDetails: Boolean = eFormula.endsWith("^")
+  val pVariables: List[ScoreCalcVariable] = parseVariables(pFormula)
+  val pResolveDetails: Boolean = pFormula.endsWith("^")
 
-  def dExpression(values: List[ScoreCalcVariable]) = renderExpression(dFormula, values.filter(_.prefix.equals("D")))
-  def eExpression(values: List[ScoreCalcVariable]) = renderExpression(eFormula, values.filter(_.prefix.equals("E")))
-  def pExpression(values: List[ScoreCalcVariable]) = renderExpression(pFormula, values.filter(_.prefix.equals("P")))
+  lazy val variables: List[ScoreCalcVariable] = dVariables ++ eVariables ++ pVariables
 
-  private def parseVariables(formula: String): List[ScoreCalcVariable] = varPattern.findAllMatchIn(formula).map{ m =>
+  def toView(values: List[ScoreCalcVariable]): ScoreCalcTemplateView = {
+    val updateVarsOfFn = updateVarsOf(values) _
+    ScoreCalcTemplateView(
+      dExpression(values), updateVarsOfFn(dVariables), dResolveDetails,
+      eExpression(values), updateVarsOfFn(eVariables), eResolveDetails,
+      pExpression(values), updateVarsOfFn(pVariables), pResolveDetails,
+      aggregateFn
+    )
+  }
+
+  def dExpression(values: List[ScoreCalcVariable]): String = renderExpression(dFormula, values.filter(_.prefix.equals("D")))
+
+  def eExpression(values: List[ScoreCalcVariable]): String = renderExpression(eFormula, values.filter(_.prefix.equals("E")))
+
+  def pExpression(values: List[ScoreCalcVariable]): String = renderExpression(pFormula, values.filter(_.prefix.equals("P")))
+
+  private def updateVarsOf(values: List[ScoreCalcVariable])(vars: List[ScoreCalcVariable]) =
+    vars.map { v =>
+      values
+        .find(vv => vv.prefix.equals(v.prefix) && vv.name.equals(v.name))
+        .map(vv => v.updated(vv.value))
+        .getOrElse(v)
+    }
+
+  private def parseVariables(formula: String): List[ScoreCalcVariable] = varPattern.findAllMatchIn(formula).map { m =>
     val prefix = m.group(1)
     val name = m.group(2)
     val scale = if (m.groupCount == 4 && !m.group(4).equals("0")) Some(m.group(4)) else None
