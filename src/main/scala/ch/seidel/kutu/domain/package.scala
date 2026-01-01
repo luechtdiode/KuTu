859f428b881e044c99cf842479d8d9636bcef547
Roland Seidel <register@interpolar.ch>
2026-01-01 13:24:33 +0100

    fix riege-assignement - add testcases

--- src/main/scala/ch/seidel/kutu/domain/package.scala
+++ src/main/scala/ch/seidel/kutu/domain/package.scala
@@ -1183,21 +1183,16 @@ package object domain {
 
     def getTeamName(extraTeams: List[String]): String = athlet.verein match {
       case Some(v) =>
-        if (wettkampf.teamrule.exists(r => r.contains("VereinGe"))) {
-          if (team == 0) v.easyprint
-          else if (team < 0 && extraTeams.size > team * -1 - 1) {
-            s"${extraTeams(team * -1 - 1)}"
-          }
-          else
-            s"${v.easyprint} $team"
-        } else {
-          if (team == 0) v.easyprint
-          else if (team < 0 && extraTeams.size > team * -1 - 1) {
-            s"${extraTeams(team * -1 - 1)}"
-          }
-          else
-            s"${v.verband.getOrElse(v.extendedprint).split(",").last.trim} $team"
+        if (team == 0) v.easyprint
+        else if (team < 0 && extraTeams.size > team * -1 - 1) {
+          s"${extraTeams(team * -1 - 1)}"
         }
+        else if (wettkampf.teamrule.exists(r => r.contains("VereinGe"))) {
+          s"${v.easyprint} $team"
+        } else if (wettkampf.hasTeams) {
+          s"${v.verband.getOrElse(v.extendedprint).split(",").last.trim} $team"
+        } else
+          s"${v.easyprint} $team"
       case _ => if (team != 0) s"$team" else ""
     }
 
