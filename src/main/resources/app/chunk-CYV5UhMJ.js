1443fe0de077a5c86f87e0413d5329bf472bab90
luechtdiode <2457748+luechtdiode@users.noreply.github.com>
2026-07-12 16:14:00 +0000

    update with generated Client from Github Actions CI for build  with [skip ci]

---
+++ src/main/resources/app/chunk-CYV5UhMJ.js
@@ -0,0 +1,1 @@
+import {k,C}from'./main-7GLE7TXE.js';var o=class extends k{show(i){return C(this,null,function*(){})}hide(i){return C(this,null,function*(){})}};export{o as SplashScreenWeb};
--- src/main/resources/app/chunk-VQCKC_2y.js
+++
@@ -1,1 +0,0 @@
-import {ch as n$1}from'./main-FHP4NNJJ.js';var n=()=>{if(n$1!==void 0)return n$1.Capacitor};export{n};
