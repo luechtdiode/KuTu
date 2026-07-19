2fcc6fc6d2569dd0ddd006d686f1557041e33bb6
luechtdiode <2457748+luechtdiode@users.noreply.github.com>
2026-07-19 00:00:13 +0000

    update with generated Client from Github Actions CI for build  with [skip ci]

---
+++ src/main/resources/app/chunk-DZ-A06tl.js
@@ -0,0 +1,1 @@
+import {ch as n$1}from'./main-NW6L32T4.js';var n=()=>{if(n$1!==void 0)return n$1.Capacitor};export{n};
--- src/main/resources/app/chunk-NTATooHg.js
+++
@@ -1,1 +0,0 @@
-import {C}from'./main-3UOLBBGD.js';var s=()=>{let e;return {lock:()=>C(null,null,function*(){let o=e,t;return e=new Promise(n=>t=n),o!==void 0&&(yield o),t})}};export{s};
