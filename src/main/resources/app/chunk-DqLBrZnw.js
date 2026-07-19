35e3beca16a5f06f3560f53f1a9dbff60b1da1b5
luechtdiode <2457748+luechtdiode@users.noreply.github.com>
2026-07-19 10:34:51 +0000

    update with generated Client from Github Actions CI for build  with [skip ci]

--- src/main/resources/app/chunk-DqLBrZnw.js
+++
@@ -1,1 +0,0 @@
-import {k,C}from'./main-2GSQ76J2.js';var o=class extends k{show(i){return C(this,null,function*(){})}hide(i){return C(this,null,function*(){})}};export{o as SplashScreenWeb};
---
+++ src/main/resources/app/chunk-DqYpIXbn.js
@@ -0,0 +1,1 @@
+import {k,C}from'./main-5LZFBZDY.js';var o=class extends k{show(i){return C(this,null,function*(){})}hide(i){return C(this,null,function*(){})}};export{o as SplashScreenWeb};
