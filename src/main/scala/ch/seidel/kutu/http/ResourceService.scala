26c2ae5f1e4f208fd1f12ecdc88f2519cffc9779
luechtdiode <register@interpolar.ch>
2026-07-29 18:15:06 +0200

    fix cache headers to invalidate new versions of index.html

--- src/main/scala/ch/seidel/kutu/http/ResourceService.scala
+++ src/main/scala/ch/seidel/kutu/http/ResourceService.scala
@@ -1,10 +1,18 @@
 package ch.seidel.kutu.http
 
 import fr.davit.pekko.http.metrics.core.scaladsl.server.HttpMetricsDirectives.*
+import org.apache.pekko.http.scaladsl.model.headers.CacheDirectives.*
+import org.apache.pekko.http.scaladsl.model.headers.`Cache-Control`
 import org.apache.pekko.http.scaladsl.server.{Directives, Route}
 
 trait ResourceService extends Directives {
-  val fallbackRoute: Route = getFromResource("app/index.html")
+  private val noCacheIndex = `Cache-Control`(`no-cache`, `no-store`, `must-revalidate`)
+
+  private val cacheAssets = `Cache-Control`(`public`, `max-age`(31536000))
+
+  val fallbackRoute: Route = respondWithHeader(noCacheIndex) {
+    getFromResource("app/index.html")
+  }
 
   private def appRoute = {
     pathPrefixLabeled("", "index") {
@@ -12,7 +20,11 @@ trait ResourceService extends Directives {
         fallbackRoute
       }
     } ~
-    getFromResourceDirectory("app") ~ getFromResourceDirectory("static")
+    respondWithHeader(cacheAssets) {
+      getFromResourceDirectory("app")
+    } ~ respondWithHeader(cacheAssets) {
+      getFromResourceDirectory("static")
+    }
   }
 
   val resourceRoutes: Route = appRoute
