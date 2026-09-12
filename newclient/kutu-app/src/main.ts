a9053f55cec1e0197d7cfef3958dd7554f118f33
luechtdiode <register@interpolar.ch>
2026-09-12 18:21:44 +0200

    cleanup deprecated angular-deps

--- newclient/kutu-app/src/main.ts
+++ newclient/kutu-app/src/main.ts
@@ -1,5 +1,5 @@
 import { enableProdMode, provideZonelessChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZonelessChangeDetection()], })
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZonelessChangeDetection()], })
   .catch(err => console.log(err));
--- newclient/kutu-app/src/test.ts
+++ newclient/kutu-app/src/test.ts
@@ -2,14 +2,14 @@
 
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+  BrowserTestingModule,
+  platformBrowserTesting
+} from '@angular/platform-browser/testing';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  BrowserTestingModule,
+  platformBrowserTesting()
 );
 
 // Then we find all the tests.
